0c039d8aebce298ba18cda8a9c92b8b77c891f3e
Igor Stepanov <sigorilla@gmail.com>
2015-10-18 22:14:04 +0300

    RTC with NTP

--- ArduinoNTPServer/ArduinoNTPServer.ino
+++ ArduinoNTPServer/ArduinoNTPServer.ino
@@ -1,74 +1,132 @@
-#include <SPI.h>
 #include <Ethernet2.h>
+#include <EthernetUdp2.h>
+#include <Wire.h>
+#include <SPI.h>
+#include "RTClib.h"
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 IPAddress ip(192, 168, 1, 177);
 
-// Initialize the Ethernet server library
-// with the IP address and port you want to use
-// (port 80 is default for HTTP):
-EthernetServer server(80);
+unsigned int NTP_PORT = 123;
 
-void setup() {
-  // Open serial communications and wait for port to open:
+const int NTP_PACKET_SIZE = 48;
+
+byte packetBuffer[ NTP_PACKET_SIZE ];
+
+EthernetUDP Udp;
+
+RTC_DS1307 RTC;
+
+int TZ = 3;
+
+DateTime dtt(__DATE__, __TIME__);
+DateTime dt(dtt.year(), dtt.month(), dtt.day(), dtt.hour() - TZ, dtt.minute(), dtt.second());
+
+unsigned long referenceTime = 0;
+unsigned long originTime = 0;
+unsigned long receiveTime = 0;
+unsigned long transmitTime = 0;
+
+void setup () {
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+    continue;
   }
-
-
-  // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
-  server.begin();
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP());
+  Udp.begin(NTP_PORT);
+
+  Wire.begin();
+  RTC.begin();
+  RTC.adjust(dt);
+  
+  Serial.println("NTP Server is running.");
 }
 
+void loop () {
+  IPAddress remoteIP;
+  int remotePort;
+  
+  int packetSize = Udp.parsePacket();
 
-void loop() {
-  // listen for incoming clients
-  EthernetClient client = server.available();
-  if (client) {
-    Serial.println("new client");
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.println("<h1>NTP Server</h1>");
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        }
-        else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-    // close the connection:
-    client.stop();
-    Serial.println("client disconnected");
+  if (packetSize) {
+    Serial.println("Get UDP packet.");
+    DateTime now = RTC.now();
+    receiveTime = now.unixtime();
+    referenceTime = receiveTime; // time of last set or update
+
+    remoteIP = Udp.remoteIP();
+    remotePort = Udp.remotePort();
+    Serial.print("IP: ");
+    Serial.println(remoteIP);
+    Serial.print("port: ");
+    Serial.println(remotePort);
+    // We've received a packet, read the data from it
+    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    // ReceiveTimestamp
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    originTime = highWord << 16 | lowWord;
+    
+    sendNTPpacket(remoteIP, remotePort);
   }
+}
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress remoteIP, int remotePort) {
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100100;   // LI, Version: 4, Mode: 4 (server)
+  packetBuffer[1] = 0b00000001;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 71; // G
+  packetBuffer[13] = 80; // P
+  packetBuffer[14] = 83; // S
+  packetBuffer[15] = 0;
+  // Reference Time
+  packetBuffer[16] = (referenceTime & 0xFF000000) >> 24;
+  packetBuffer[17] = (referenceTime & 0x00FF0000) >> 16;
+  packetBuffer[18] = (referenceTime & 0x0000FF00) >> 8;
+  packetBuffer[19] = (referenceTime & 0x000000FF);
+  // 20-23 fractions
+
+  // Origin Time
+  packetBuffer[24] = (originTime & 0xFF000000) >> 24;
+  packetBuffer[25] = (originTime & 0x00FF0000) >> 16;
+  packetBuffer[26] = (originTime & 0x0000FF00) >> 8;
+  packetBuffer[27] = (originTime & 0x000000FF);
+  // 28-31 fractions
+
+  // Receive Time
+  packetBuffer[32] = (receiveTime & 0xFF000000) >> 24;
+  packetBuffer[33] = (receiveTime & 0x00FF0000) >> 16;
+  packetBuffer[34] = (receiveTime & 0x0000FF00) >> 8;
+  packetBuffer[35] = (receiveTime & 0x000000FF);
+  // 36-39 fractions
+
+  // Transmit Time
+  DateTime now = RTC.now();
+  transmitTime = now.unixtime();
+  packetBuffer[40] = (transmitTime & 0xFF000000) >> 24;
+  packetBuffer[41] = (transmitTime & 0x00FF0000) >> 16;
+  packetBuffer[42] = (transmitTime & 0x0000FF00) >> 8;
+  packetBuffer[43] = (transmitTime & 0x000000FF);
+  // 44-47 fractions
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(remoteIP, remotePort);
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
 }
